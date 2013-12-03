6c0fad8dd113a641e63744f5314ecf781577d5a0
xodet <xodetk@gmail.com>
2013-12-03 01:00:55 +0100

    Threshold using HSV image (autocalibrate not implemented, yet)

--- mmcv/mmcv/Form1.Designer.cs
+++ mmcv/mmcv/Form1.Designer.cs
@@ -33,6 +33,23 @@
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
             this.imageBox2 = new Emgu.CV.UI.ImageBox();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panelHsvSettings = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -42,6 +59,14 @@
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
             this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panelHsvSettings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -52,7 +77,7 @@
             this.panel1.Controls.Add(this.splitContainer1);
             this.panel1.Location = new System.Drawing.Point(6, 6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(544, 226);
+            this.panel1.Size = new System.Drawing.Size(606, 226);
             this.panel1.TabIndex = 0;
             // 
             // splitContainer1
@@ -70,8 +95,8 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.imageBox2);
-            this.splitContainer1.Size = new System.Drawing.Size(544, 226);
-            this.splitContainer1.SplitterDistance = 271;
+            this.splitContainer1.Size = new System.Drawing.Size(606, 226);
+            this.splitContainer1.SplitterDistance = 301;
             this.splitContainer1.SplitterWidth = 2;
             this.splitContainer1.TabIndex = 0;
             // 
@@ -82,7 +107,7 @@
             this.imageBox1.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
             this.imageBox1.Location = new System.Drawing.Point(0, 0);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(271, 226);
+            this.imageBox1.Size = new System.Drawing.Size(301, 226);
             this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.imageBox1.TabIndex = 6;
             this.imageBox1.TabStop = false;
@@ -94,7 +119,7 @@
             this.imageBox2.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
             this.imageBox2.Location = new System.Drawing.Point(0, 0);
             this.imageBox2.Name = "imageBox2";
-            this.imageBox2.Size = new System.Drawing.Size(271, 226);
+            this.imageBox2.Size = new System.Drawing.Size(303, 226);
             this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.imageBox2.TabIndex = 7;
             this.imageBox2.TabStop = false;
@@ -103,16 +128,267 @@
             // 
             this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.groupBox1);
             this.panel2.Controls.Add(this.button1);
             this.panel2.Location = new System.Drawing.Point(6, 238);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(544, 183);
+            this.panel2.Size = new System.Drawing.Size(606, 183);
             this.panel2.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.panelHsvSettings);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(231, 177);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "calibrate recognizing";
+            // 
+            // panelHsvSettings
+            // 
+            this.panelHsvSettings.Controls.Add(this.button4);
+            this.panelHsvSettings.Controls.Add(this.button3);
+            this.panelHsvSettings.Controls.Add(this.label5);
+            this.panelHsvSettings.Controls.Add(this.numericUpDown5);
+            this.panelHsvSettings.Controls.Add(this.label6);
+            this.panelHsvSettings.Controls.Add(this.numericUpDown6);
+            this.panelHsvSettings.Controls.Add(this.label3);
+            this.panelHsvSettings.Controls.Add(this.numericUpDown3);
+            this.panelHsvSettings.Controls.Add(this.label4);
+            this.panelHsvSettings.Controls.Add(this.numericUpDown4);
+            this.panelHsvSettings.Controls.Add(this.label2);
+            this.panelHsvSettings.Controls.Add(this.numericUpDown2);
+            this.panelHsvSettings.Controls.Add(this.label1);
+            this.panelHsvSettings.Controls.Add(this.numericUpDown1);
+            this.panelHsvSettings.Enabled = false;
+            this.panelHsvSettings.Location = new System.Drawing.Point(8, 50);
+            this.panelHsvSettings.Name = "panelHsvSettings";
+            this.panelHsvSettings.Size = new System.Drawing.Size(217, 121);
+            this.panelHsvSettings.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(53, 86);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 28;
+            this.button4.Text = "Load preset";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(134, 86);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 27;
+            this.button3.Text = "Save preset";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(130, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "to";
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Location = new System.Drawing.Point(152, 56);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown5.TabIndex = 25;
+            this.numericUpDown5.Value = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown5.ValueChanged += new System.EventHandler(this.numericUpDown5_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Value:";
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Location = new System.Drawing.Point(67, 56);
+            this.numericUpDown6.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown6.TabIndex = 23;
+            this.numericUpDown6.Value = new decimal(new int[] {
+            96,
+            0,
+            0,
+            0});
+            this.numericUpDown6.ValueChanged += new System.EventHandler(this.numericUpDown6_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(130, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "to";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Location = new System.Drawing.Point(152, 30);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown3.TabIndex = 21;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            172,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Saturation:";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Location = new System.Drawing.Point(67, 30);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown4.TabIndex = 19;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            92,
+            0,
+            0,
+            0});
+            this.numericUpDown4.ValueChanged += new System.EventHandler(this.numericUpDown4_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "to";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Location = new System.Drawing.Point(152, 4);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown2.TabIndex = 17;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            14,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Hue:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(67, 4);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown1.TabIndex = 15;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 25);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Calibrate HSV treshold";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(441, 144);
+            this.button1.Location = new System.Drawing.Point(503, 144);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(97, 33);
             this.button1.TabIndex = 0;
@@ -124,7 +400,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(556, 427);
+            this.ClientSize = new System.Drawing.Size(618, 427);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -138,6 +414,15 @@
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
             this.panel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.panelHsvSettings.ResumeLayout(false);
+            this.panelHsvSettings.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -150,6 +435,23 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button button1;
         private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panelHsvSettings;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
 
     }
 }
--- mmcv/mmcv/Form1.cs
+++ mmcv/mmcv/Form1.cs
@@ -24,6 +24,13 @@ namespace mmcv
         private int FPS = 0;
         //end.
 
+        //switch to HSV calibrate
+        private bool calibrationHSVLevel = false;
+        private double hue_min = 0, hue_max = 14;
+        private double saturation_min = 92, saturation_max = 172;
+        private double value_min = 96, value_max = 255;
+        //end.
+
         public Form1()
         {
             InitializeComponent();
@@ -43,36 +50,84 @@ namespace mmcv
             }
 
             imageFrame = imageFrame.Flip(Emgu.CV.CvEnum.FLIP.HORIZONTAL);
+            Image<Hsv, Byte> hsvFrame = imageFrame.Convert<Hsv, Byte>();
 
-            //--just play with contour display--
-            Image<Gray, byte> grayFrame = imageFrame
-               .Convert<Gray, byte>()                               //to gray image
-               .ThresholdBinary(new Gray(127), new Gray(255))       //first treshold to get only part of interesting pixels
-               .SmoothGaussian(11)                                  //gaussian filter to make smooth black & white pixels on edges
-               .ThresholdBinary(new Gray(127), new Gray(255))       //once again treshold to make nice edges (thanks to gauss)
-               .Dilate(3);                                          //enlarge dots
-
-            Image<Gray, byte> finalFrame = new Image<Gray, byte>(grayFrame.Width, grayFrame.Height, new Gray(255));
-
-            Contour<Point> contours = grayFrame
-                .FindContours(Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_LINK_RUNS, Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_TREE);
-
-            if (contours != null)
+            if (calibrationHSVLevel)
             {
-                finalFrame.Draw(
-                    contours, new Gray(0), new Gray(255), 1, 2, new Point(0, 0)
-                );
-
-                imageFrame.Draw(
-                    contours, new Bgr(Color.White), new Bgr(Color.Black), 1, 2, new Point(0, 0)
-                );
+                CalibrateHSV(ref hsvFrame);
             }
-            //--end--
+            else
+            {
+
+                /*/--just play with contour display--
+                Image<Gray, byte> grayFrame = imageFrame
+                   .Convert<Gray, byte>()                               //to gray image
+                   .ThresholdBinary(new Gray(127), new Gray(255))       //first treshold to get only part of interesting pixels
+                   .SmoothGaussian(11)                                  //gaussian filter to make smooth black & white pixels on edges
+                   .ThresholdBinary(new Gray(127), new Gray(255))       //once again treshold to make nice edges (thanks to gauss)
+                   .Dilate(3);                                          //enlarge dots
+
+                Image<Gray, byte> finalFrame = new Image<Gray, byte>(grayFrame.Width, grayFrame.Height, new Gray(255));
+
+                Contour<Point> contours = grayFrame
+                    .FindContours(Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_LINK_RUNS, Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_TREE);
+
+                if (contours != null)
+                {
+                    finalFrame.Draw(
+                        contours, new Gray(0), new Gray(255), 1, 2, new Point(0, 0)
+                    );
+
+                    imageFrame.Draw(
+                        contours, new Bgr(Color.White), new Bgr(Color.Black), 1, 2, new Point(0, 0)
+                    );
+                }
+                //--end--
+
+                addFPS(imageFrame, 10, 30);
+
+                imageBox2.Image = finalFrame;
+                imageBox1.Image = imageFrame;*/ 
+            }
 
             addFPS(imageFrame, 10, 30);
 
-            imageBox2.Image = finalFrame;
             imageBox1.Image = imageFrame;
+            imageBox2.Image = hsvFrame;
+        }
+
+        private void CalibrateHSV(ref Image<Hsv, Byte> hsvImage)
+        {
+            float horizontalFactor = 0.2f;
+            float verticalFactor = 0.2f;
+
+            int rectWidth = (int)(hsvImage.Width * horizontalFactor);
+            int rectHeight = (int)(hsvImage.Height * verticalFactor);
+
+            int topLeftX = (int)((((float)hsvImage.Width / 2) - rectWidth) / 2);
+            int topLeftY = (int)(((float)hsvImage.Height - rectHeight) / 2);
+
+            Rectangle rangeOfInterest = new Rectangle(topLeftX, topLeftY, rectWidth, rectHeight);
+
+            //CircleF circle = new CircleF(new PointF(hsvImage.Width / 5, hsvImage.Height / 2), 50);
+
+            hsvImage.Draw(rangeOfInterest, new Hsv(255,255,255), 3);
+
+            Image<Gray, Byte> maskedImage = hsvImage.InRange(
+                new Hsv(hue_min, saturation_min, value_min),
+                new Hsv(hue_max, saturation_max, value_max));
+
+            hsvImage = maskedImage.Convert<Hsv, Byte>() //tu powstaje jakiś "First chance of exception..."
+                .SmoothGaussian(5)
+                .Dilate(1)
+                .Convert<Rgb, Byte>()
+                .ThresholdBinary(new Rgb(127,127,127), new Rgb(255,255,255))
+                .Convert<Hsv, Byte>();
+
+            hsvImage.Draw(rangeOfInterest, new Hsv(255, 255, 255), 3);
+
+            //hsvImage.ROI = rangeOfInterest;
+
         }
 
         private void addFPS(Image<Bgr, Byte> imageFrame, int x, int y)
@@ -134,7 +189,43 @@ namespace mmcv
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            button1.PerformClick();
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            calibrationHSVLevel = !calibrationHSVLevel;
+            panelHsvSettings.Enabled = calibrationHSVLevel;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            hue_min = Convert.ToDouble((sender as NumericUpDown).Value);
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            hue_max = Convert.ToDouble((sender as NumericUpDown).Value);
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+            saturation_min = Convert.ToDouble((sender as NumericUpDown).Value);
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            saturation_max = Convert.ToDouble((sender as NumericUpDown).Value);
+        }
+
+        private void numericUpDown6_ValueChanged(object sender, EventArgs e)
+        {
+            value_min = Convert.ToDouble((sender as NumericUpDown).Value);
+        }
+
+        private void numericUpDown5_ValueChanged(object sender, EventArgs e)
+        {
+            value_max = Convert.ToDouble((sender as NumericUpDown).Value);
         }
     }
 }
